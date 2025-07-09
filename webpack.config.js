4727ae116d84a9f892e5a144adae0e380ef4117c
Adam Stack <stackadamj@gmail.com>
2025-07-09 11:59:47 -0500

    Fix deployment structure and relative paths
    - Update webpack to use relative publicPath for subdirectory deployment
    - Fix .cpanel.yml to deploy files directly to qual root
    - Add .htaccess for proper routing and MIME types
    - Rebuild with new bundle hash

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'web/build'),
     filename: 'bundle.[contenthash].js',
-    publicPath: '/',
+    publicPath: './',
     clean: true,
   },
   module: {
