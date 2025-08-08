c7cc5a844e217c39769b4963b304c8c5f0892b57
Adam Stack <stackadamj@gmail.com>
2025-08-08 16:23:23 -0500

    Add comprehensive React Native web polyfills - fix all missing modules

--- src/utils/polyfills.web.js
+++ src/utils/polyfills.web.js
@@ -1,15 +1,16 @@
 // Polyfills for web environment
 
 // Mock Platform - CRITICAL for React Native web
-if (typeof window !== 'undefined' && !window.Platform) {
-  window.Platform = {
+if (typeof window !== 'undefined') {
+  // Platform must be global for React Native
+  global.Platform = window.Platform = {
     OS: 'web',
     Version: 1,
     isPad: false,
     isTV: false,
     isTVOS: false,
     isTesting: false,
-    select: (obj) => obj.web || obj.default,
+    select: (obj) => obj.web || obj.default || Object.values(obj)[0],
   };
 }
 
@@ -19,70 +20,122 @@ global.__fbBatchedBridgeConfig = {
   localModulesConfig: [],
 };
 
-// Mock NativeModules
+// Complete NativeModules mock
 global.NativeModules = {
   SourceCode: {
     scriptURL: 'http://localhost:8081/index.bundle?platform=web',
+    getConstants: () => ({
+      scriptURL: 'http://localhost:8081/index.bundle?platform=web',
+    }),
   },
   PlatformConstants: {
     getConstants: () => ({
       reactNativeVersion: { major: 0, minor: 72, patch: 0 },
+      isTesting: false,
+      forceTouchAvailable: false,
     }),
   },
   DeviceInfo: {
+    getConstants: () => ({
+      Dimensions: {
+        window: {
+          width: window.innerWidth || 1024,
+          height: window.innerHeight || 768,
+          scale: 1,
+          fontScale: 1,
+        },
+        screen: {
+          width: window.screen?.width || window.innerWidth || 1024,
+          height: window.screen?.height || window.innerHeight || 768,
+          scale: 1,
+          fontScale: 1,
+        },
+      },
+      isTablet: false,
+      isEmulator: false,
+    }),
     Dimensions: {
       window: {
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: window.innerWidth || 1024,
+        height: window.innerHeight || 768,
         scale: 1,
         fontScale: 1,
       },
       screen: {
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: window.screen?.width || window.innerWidth || 1024,
+        height: window.screen?.height || window.innerHeight || 768,
         scale: 1,
         fontScale: 1,
       },
     },
   },
+  UIManager: {
+    RCTView: {
+      directEventTypes: {},
+    },
+    getViewManagerConfig: () => null,
+  },
+  KeyboardObserver: {
+    addListener: () => ({ remove: () => {} }),
+  },
+  StatusBarManager: {
+    HEIGHT: 0,
+    getHeight: (callback) => callback({ height: 0 }),
+  },
+  Networking: {
+    addListener: () => {},
+    removeListeners: () => {},
+  },
+  AppState: {
+    getCurrentAppState: (callback) => callback({ app_state: 'active' }),
+    addListener: () => ({ remove: () => {} }),
+  },
+  Clipboard: {
+    setString: () => {},
+    getString: () => Promise.resolve(''),
+  },
+  I18nManager: {
+    localeIdentifier: 'en_US',
+    getConstants: () => ({
+      isRTL: false,
+      doLeftAndRightSwapInRTL: true,
+      localeIdentifier: 'en_US',
+    }),
+  },
+  Linking: {
+    getInitialURL: () => Promise.resolve(null),
+    addEventListener: () => ({ remove: () => {} }),
+    canOpenURL: () => Promise.resolve(true),
+    openURL: (url) => window.open(url, '_blank'),
+  },
+  Settings: {
+    get: () => ({}),
+    set: () => {},
+    watchKeys: () => {},
+  },
+  Appearance: {
+    getColorScheme: () => 'light',
+    addChangeListener: () => ({ remove: () => {} }),
+  },
+  AccessibilityInfo: {
+    addEventListener: () => ({ remove: () => {} }),
+    isScreenReaderEnabled: () => Promise.resolve(false),
+    fetch: () => Promise.resolve(false),
+  },
 };
 
-// Mock TurboModuleRegistry
+// Mock TurboModuleRegistry with ALL modules
 global.__turboModuleProxy = function(name) {
-  const mockModule = {
-    SourceCode: {
-      getConstants: () => ({
-        scriptURL: 'http://localhost:8081/index.bundle?platform=web',
-      }),
-    },
-    PlatformConstants: {
-      getConstants: () => ({
-        reactNativeVersion: { major: 0, minor: 72, patch: 0 },
-        isTesting: false,
-        forceTouchAvailable: false,
-      }),
-    },
-    DeviceInfo: {
-      getConstants: () => ({
-        Dimensions: {
-          window: {
-            width: window.innerWidth,
-            height: window.innerHeight,
-            scale: 1,
-            fontScale: 1,
-          },
-          screen: {
-            width: window.innerWidth,
-            height: window.innerHeight,
-            scale: 1,
-            fontScale: 1,
-          },
-        },
-      }),
-    },
-  };
+  // Return NativeModules entry if it exists
+  if (global.NativeModules[name]) {
+    return global.NativeModules[name];
+  }
   
-  return mockModule[name] || {};
+  // Otherwise return a generic mock
+  return {
+    getConstants: () => ({}),
+    addListener: () => ({ remove: () => {} }),
+  };
 };
 
 // setImmediate polyfill
