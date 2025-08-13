9f23e8e49be7af86b012417d58e191210c36d7fe
Adam Stack <stackadamj@gmail.com>
2025-08-12 19:23:44 -0500

    Deploy to qual: v2025.08.12.12

--- src/utils/version.js
+++ src/utils/version.js
@@ -121,4 +121,4 @@ export const setVersion = async (version) => {
 
 // Export a static version for builds
 // This will be updated by the build script
-export const BUILD_VERSION = '2025.08.12.9';
+export const BUILD_VERSION = '2025.08.12.12';
