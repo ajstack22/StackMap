a293c1930920f052912dff46d0540011a3ff6802
Adam Stack <stackadamj@gmail.com>
2025-08-16 09:01:29 -0500

    2025.08.16.4 - Deployment version bump

--- src/utils/version.js
+++ src/utils/version.js
@@ -121,5 +121,5 @@ export const setVersion = async (version) => {
 
 // Export a static version for builds
 // This will be updated by the build script
-export const BUILD_VERSION = '2025.08.16.3';
+export const BUILD_VERSION = '2025.08.16.4';
 // Clean deployment system fully operational - 2025-01-13
