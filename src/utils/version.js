ca32f93e10acccd598d3485ec659c1a3a5900379
Adam Stack <stackadamj@gmail.com>
2025-09-16 00:07:35 -0500

    2025.09.16.1 - S-DEBT-003 Console Cleanup Analysis and Backlog Update

--- src/utils/version.js
+++ src/utils/version.js
@@ -118,5 +118,5 @@ export const setVersion = async version => {
 
 // Export a static version for builds
 // This will be updated by the build script
-export const BUILD_VERSION = '2025.09.15.3';
+export const BUILD_VERSION = '2025.09.16.1';
 // Clean deployment system fully operational - 2025-01-13
