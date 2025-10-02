7f180a2f82375391848481c765c9e6c2b2a95945
Adam Stack <stackadamj@gmail.com>
2025-10-02 18:37:12 -0500

    2025.10.02.5 - Fix 26 lint errors blocking deployment - all false positives with clear documentation
    Fix 26 lint errors blocking deployment - all false positives with clear documentation
    
    ### Deployment Date: 2025-10-02_18:37:12

--- src/utils/version.js
+++ src/utils/version.js
@@ -118,5 +118,5 @@ export const setVersion = async version => {
 
 // Export a static version for builds
 // This will be updated by the build script
-export const BUILD_VERSION = '2025.10.02.4';
+export const BUILD_VERSION = '2025.10.02.5';
 // Clean deployment system fully operational - 2025-01-13
